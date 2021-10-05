252cbd14bf80f4f7f9d5331f101a99ff41d02c2c
Jdb443 <sholom@sbswireless.com>
2021-10-05 16:18:21 -0400

    Api and Model

---
+++ PPTurl/PPTurl/HomeVC.swift
@@ -0,0 +1,31 @@
+//
+//  HomeVC.swift
+//  PPTurl
+//
+//  Created by mac on 10/6/21.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    var episodes : [BuzzsproutEpisode] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
